e063b8c75eb293b36e4aee5d4c7750a5ec31f08a
karodehemant9 <karode.hemant9@gmail.com>
2023-10-19 11:25:51 +0530

    added login page

---
+++ Full Stack Expense Tracker 1 frontend/login.js
@@ -0,0 +1,50 @@
+const newUserButton = document.getElementById('new-user-button');
+newUserButton.addEventListener('click', newUserHandling);
+
+function newUserHandling(e){
+    //redirecting to signup page
+    const destinationURL = "signup.html";
+    // Redirect to the destination page
+    window.location.href = destinationURL;     
+}
+
+
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler(e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (email === null || password === null) {
+        alert('Please fill in all fields with valid data.');
+    } else {
+        console.log(email);
+        console.log(password);
+        loginUser(email, password);
+        // Reset the form
+        e.target.reset();
+    }
+
+}
+
+function loginUser(email, password){
+    user = {email, password};
+    loginUserOnServer(user);
+}
+
+function loginUserOnServer(user)
+{
+    axios.post("http://localhost:8000/user/login", user)
+    .then(response =>{
+        if(response.success === true){
+            // Assume you receive a successful response from the backend
+            localStorage.setItem('token', response.data.token)
+            window.location.href = "ExpenseTracker.html"; 
+        }
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+}
